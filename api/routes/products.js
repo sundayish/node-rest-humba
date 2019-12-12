4f78f91280f3103f2143c3fab124683e8866e7b8
Tom Sonntag <tm.snntag@icloud.com>
2019-12-12 15:33:38 +0100

    set up mongoose and body-parser

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+//GETing from a server
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling request GET for /preoducs'
+        message: 'Handling request GET for /products'
     });
 });
 
+//POSTing into the server
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
     res.status(201).json({
-        message: 'Handling request POST for /preoducs'
+        message: 'Handling request POST for /products',
+        createdProduct: product
     });
 });
 
+//Defining non-static ID
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     if(id === 'special'){
